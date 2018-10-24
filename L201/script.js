397338be0bee18b6d44f76d376dd83f8504bea06
maxo1z <maxinio55@gmail.com>
2018-10-24 16:27:45 +0400

    l201

--- L201/script.js
+++ L201/script.js
@@ -1,6 +1,18 @@
 var year = prompt('რომელ წელს გამოვიდა angular js 2.0?','');
 
-if(year !=2016) {
 
-alert('პასუხი არასწორია!');
+if(year> 0 ) {
+
+alert(1);
+
 }
+
+else if(year<0) {
+    
+        alert(-1);
+    }
+    else  {
+        alert(0);
+    }
+    
+    
